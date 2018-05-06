2362d5e3cc188809c9b2b0d5636c88ca7cdaeff0
Ou-Bingqin <hz19980316@qq.com>
2018-05-07 03:37:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/plenkuing/androidevent/CallbackFrag.java
@@ -0,0 +1,19 @@
+package com.example.plenkuing.androidevent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+/**
+ * Created by PlenKuing on 2018/5/5.
+ */
+
+public class CallbackFrag extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.callback_fg);
+    }
+}
+
